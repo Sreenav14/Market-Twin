8c70f1baf6c8c88af8e7d29cdb995fd3181947d2
Sreenav14 <sreenavjajerao@gmail.com>
2026-08-30 12:09:39 -0500

    style(web): load dedicated auth styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import { App } from "./app/App";
 import "./styles.css";
+import "./auth.css";
 
 const rootElement = document.getElementById("root");
 
