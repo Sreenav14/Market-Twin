c38ec319f42fbb80433eccd732e1ce892d8d194d
Sreenav14 <sreenavjajerao@gmail.com>
2026-09-11 01:46:02 -0500

    fix(web): forward input refs

--- apps/web/src/components/ui/input.tsx
+++ apps/web/src/components/ui/input.tsx
@@ -1,6 +1,8 @@
-import type { InputHTMLAttributes } from "react";
+import { forwardRef, type InputHTMLAttributes } from "react";
 import { cn } from "../../lib/utils";
 
-export function Input({ className, ...props }: InputHTMLAttributes<HTMLInputElement>) {
-  return <input className={cn("text-input", className)} {...props} />;
-}
+export const Input = forwardRef<HTMLInputElement, InputHTMLAttributes<HTMLInputElement>>(
+  function Input({ className, ...props }, ref) {
+    return <input ref={ref} className={cn("text-input", className)} {...props} />;
+  },
+);
